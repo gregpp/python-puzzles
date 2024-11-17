33a791934fbcf64397582a86728a162e17dda24c
Greg <63721435+gregpp@users.noreply.github.com>
2024-11-17 18:24:30 +0100

    Update product.py

--- product.py
+++ product.py
@@ -1,10 +1,8 @@
+#This script finds all unique products for given list of integers
 l = [-1,2,2,3,6]
-#l = list(set(l))
 s = set()
 ln = len(l)
 for i in range(ln-1):
-    #print("i=",i)
     for j in range(i+1, ln):
-        #print("j=",j)
         s.add(l[i]*l[j])
 print (s)
